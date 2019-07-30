81d78b277da4f31ed4ad3388c439f15dfd7f9383
William <williamstylite@gmail.com>
2019-07-30 17:00:46 -0500

    Update VoiceStateUpdate for just JDA

--- src/main/java/bot/bricolo/granite/entities/payload/VoiceServerUpdate.java
+++ src/main/java/bot/bricolo/granite/entities/payload/VoiceServerUpdate.java
@@ -4,20 +4,21 @@ import bot.bricolo.granite.entities.IJsonSerializable;
 import com.mewna.catnip.entity.user.User;
 import com.mewna.catnip.entity.user.VoiceState;
 import net.dv8tion.jda.api.hooks.VoiceDispatchInterceptor;
+import net.dv8tion.jda.api.utils.data.DataObject;
 import org.json.JSONObject;
 
 public class VoiceServerUpdate implements IJsonSerializable {
     private final String sessionId;
     private final String guildId;
-    private final JSONObject event;
+    private final DataObject event;
 
     public VoiceServerUpdate(VoiceDispatchInterceptor.VoiceServerUpdate update) {
         this.sessionId = update.getSessionId();
         this.guildId = update.getGuildId();
-        this.event = update.getJSON().getJSONObject("d");
+        this.event = update.toData().getObject("d");
     }
 
-    public VoiceServerUpdate(com.mewna.catnip.entity.voice.VoiceServerUpdate update) {
+   /* public VoiceServerUpdate(com.mewna.catnip.entity.voice.VoiceServerUpdate update) {
         User self = update.catnip().selfUser();
         assert self != null;
         VoiceState voiceState = update.catnip().cache().voiceState(update.guildId(), self.id());
@@ -25,8 +26,8 @@ public class VoiceServerUpdate implements IJsonSerializable {
 
         this.sessionId = voiceState.sessionId();
         this.guildId = update.guildId();
-        this.event = new JSONObject(update.toJson().getJsonObject("d").encode());
-    }
+        //this.event = new JSONObject(update.toJson().getJsonObject("d").encode());
+    } */
 
     @Override
     public JSONObject toJson() {
